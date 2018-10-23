d701eeb5bab9d260f3b7317c367ff99da4e3d01a
J-Fo-S <sherm_jonathan@hotmail.com>
2018-10-23 18:05:36 +0800

    mlp_boilerplate

--- midterm_project/data_load.py
+++ midterm_project/data_load.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 def add_arguments(parser):
     parser.add_argument("--personify", type=str, default=False, help="match sentiment to speaker")
@@ -67,11 +67,21 @@ def remove_data_classes(data):
 
     return data_remove
 
-def data_split(data):
-    #to do
-    return
+def text_label_vector(data):
+    tlv = get_annotations(data)
+    tlv = [list(s) for s in tlv]
+    #convert to int
+    tlv = [[int(y) for y in x] for x in tlv]
+    tlv = np.asarray(tlv)
+    #reduce 3 removed data columns
+    tlv = np.delete(tlv, np.s_[:-4], axis=1)
+    labels = np.asarray(get_emotions(data))
+    labels = labels[:,np.newaxis]
+    tlv_one = np.expand_dims(np.max(tlv,axis=1),axis=1)
+    labels = np.concatenate((tlv_one,labels),axis=1)
+    return tlv, tlv_one, labels
 
-
+# the following "get" funcs return a vector of each attribute
 def get_speakers(data):
     speakers = []
     for r in range(len(data)):
@@ -97,12 +107,12 @@ def get_emotions(data):
     return emotions
 
 def get_annotations(data):
-    emotions = []
+    annotations = []
     for r in range(len(data)):
         #for c in range(len(data[r])):
         #print(data[r]['utterance'])
         annotations.append(data[r]['annotation'])
-    return utterances
+    return annotations
 
 def get_num_words_per_sample(data):
     """Returns the median number of words per sample given corpus.
@@ -117,6 +127,10 @@ def get_num_words_per_sample(data):
     words_median = np.asarray([len(s) for s in word_lines])
     words_median = np.median(words_median)
     plot_sample_length_distribution(word_lines, words_median)
+    plot_frequency_distribution_of_ngrams(word_lines, (1,1))
+    plot_frequency_distribution_of_ngrams(word_lines, (2,2))
+    plot_frequency_distribution_of_ngrams(word_lines, (3,3))
+    plot_frequency_distribution_of_ngrams(word_lines, (4,4))
 
     print("median words per utterance %.1f" % (words_median))
     #return np.median([len(s.split()) for s in num_words])
@@ -128,6 +142,54 @@ def get_class_distribution(data):
     plot_class_distribution(count_map)
     print("samples by emotion class %s " % (list(count_map.most_common())))
 
+def plot_frequency_distribution_of_ngrams(sample_texts,
+                                          ngram_range=(1, 2),
+                                          num_ngrams=50):
+    """Plots the frequency distribution of n-grams.
+    # Arguments
+        samples_texts: list, sample texts.
+        ngram_range: tuple (min, mplt), The range of n-gram values to consider.
+            Min and mplt are the lower and upper bound values for the range.
+        num_ngrams: int, number of n-grams to plot.
+            Top `num_ngrams` frequent n-grams will be plotted.
+    """
+    # Create args required for vectorizing.
+    kwargs = {
+            'ngram_range': ngram_range,
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': 'word',  # Split text into word tokens.
+    }
+    vectorizer = CountVectorizer(**kwargs)
+
+    # This creates a vocabulary (dict, where keys are n-grams and values are
+    # idxices). This also converts every text to an array the length of
+    # vocabulary, where every element idxicates the count of the n-gram
+    # corresponding at that idxex in vocabulary.
+    vectorized_texts = vectorizer.fit_transform(sample_texts)
+
+    # This is the list of all n-grams in the index order from the vocabulary.
+    all_ngrams = list(vectorizer.get_feature_names())
+    num_ngrams = min(num_ngrams, len(all_ngrams))
+    # ngrams = all_ngrams[:num_ngrams]
+
+    # Add up the counts per n-gram ie. column-wise
+    all_counts = vectorized_texts.sum(axis=0).tolist()[0]
+
+    # Sort n-grams and counts by frequency and get top `num_ngrams` ngrams.
+    all_counts, all_ngrams = zip(*[(c, n) for c, n in sorted(
+        zip(all_counts, all_ngrams), reverse=True)])
+    ngrams = list(all_ngrams)[:num_ngrams]
+    counts = list(all_counts)[:num_ngrams]
+
+    idx = np.arange(num_ngrams)
+    plt.bar(idx, counts, width=0.8, color='b')
+    plt.xlabel('N-grams')
+    plt.ylabel('Frequencies')
+    plt.title('Frequency distribution of n-grams where n is %d' % (ngram_range[0]))
+    plt.xticks(idx, ngrams, rotation=45)
+    plt.show()
 
 def plot_sample_length_distribution(word_lines, words_median):
     """Plots the sample length distribution.
---
+++ midterm_project/mlp.py
@@ -0,0 +1,101 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from tensorflow.python.keras import models
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dropout
+
+# Vectorization parameters
+# Range (inclusive) of n-gram sizes for tokenizing text.
+NGRAM_RANGE = (1, 2)
+
+# Limit on the number of features. We use the top 20K features.
+TOP_K = 20000
+
+# Whether text should be split into word or character n-grams.
+# One of 'word', 'char'.
+TOKEN_MODE = 'word'
+
+# Minimum document/corpus frequency below which a token will be discarded.
+MIN_DOCUMENT_FREQUENCY = 2
+
+def ngram_vectorize(train_texts, train_labels, val_texts):
+    """Vectorizes texts as n-gram vectors.
+
+    1 text = 1 tf-idf vector the length of vocabulary of unigrams + bigrams.
+
+    # Arguments
+        train_texts: list, training text strings.
+        train_labels: np.ndarray, training labels.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val: vectorized training and validation texts
+    """
+    # Create keyword arguments to pass to the 'tf-idf' vectorizer.
+    kwargs = {
+            'ngram_range': NGRAM_RANGE,  # Use 1-grams + 2-grams.
+            'dtype': 'int32',
+            'strip_accents': 'unicode',
+            'decode_error': 'replace',
+            'analyzer': TOKEN_MODE,  # Split text into word tokens.
+            'min_df': MIN_DOCUMENT_FREQUENCY,
+    }
+    vectorizer = TfidfVectorizer(**kwargs)
+
+    # Learn vocabulary from training texts and vectorize training texts.
+    x_train = vectorizer.fit_transform(train_texts)
+
+    # Vectorize validation texts.
+    x_val = vectorizer.transform(val_texts)
+
+    # Select top 'k' of the vectorized features.
+    selector = SelectKBest(f_classif, k=min(TOP_K, x_train.shape[1]))
+    selector.fit(x_train, train_labels)
+    x_train = selector.transform(x_train).astype('float32')
+    x_val = selector.transform(x_val).astype('float32')
+    return x_train, x_val
+
+
+def mlp_model(layers, units, dropout_rate, input_shape, num_classes):
+    """Creates an instance of a multi-layer perceptron model.
+
+    # Arguments
+        layers: int, number of `Dense` layers in the model.
+        units: int, output dimension of the layers.
+        dropout_rate: float, percentage of input to drop at Dropout layers.
+        input_shape: tuple, shape of input to the model.
+        num_classes: int, number of output classes.
+
+    # Returns
+        An MLP model instance.
+    """
+    op_units, op_activation = _get_last_layer_units_and_activation(num_classes)
+    model = models.Sequential()
+    model.add(Dropout(rate=dropout_rate, input_shape=input_shape))
+
+    for _ in range(layers-1):
+        model.add(Dense(units=units, activation='relu'))
+        model.add(Dropout(rate=dropout_rate))
+
+    model.add(Dense(units=op_units, activation=op_activation))
+    return model
+
+
+    def _get_last_layer_units_and_activation(num_classes):
+    """Gets the # units and activation function for the last network layer.
+
+    # Arguments
+        num_classes: int, number of classes.
+
+    # Returns
+        units, activation values.
+    """
+    if num_classes == 2:
+        activation = 'sigmoid'
+        units = 1
+    else:
+        activation = 'softmax'
+        units = num_classes
+    return units, activation
+
